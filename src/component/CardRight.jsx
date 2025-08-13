c9fd79148940b81a527d6e6b2c128504742923e9
dilnura <dilnura@gmail.com>
2025-08-13 20:52:20 +0500

    feat: update CardRight component with conditional rendering and improved styling for cart items

--- src/component/CardRight.jsx
+++ src/component/CardRight.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import "./CardRight.css";
 function CardRight({ desserts }) {
   if (!desserts || desserts.length === 0) return null;
-  const count = useSelector((store) => store.counter.value);
+  const count = useSelector((store) => store.counter.value) || 0;
 
   return (
     <div className="cardRight">
-      <h1>Your cart ({count})</h1>
-      {desserts.map((dessert) => (
-        <div key={dessert.id} className="cart-item">
-          <img src={dessert.image.thumbnail} alt={dessert.name} width="50" />
-          <p>
-            {dessert.quantity} * {dessert.price}$
-          </p>
-          <span>{dessert.name}</span>
-          <span>{dessert.price * dessert.quantity}$</span>
+      {count === 0 ? (
+        <div>
+          <h1>Your cart ({count})</h1>
         </div>
-      ))}
+      ) : (
+        <div>
+          <h1>Your cart ({count})</h1>
+          {desserts.map((dessert) => (
+            <div key={dessert.id} className="cart-item">
+              <img src={dessert.image.thumbnail} alt={dessert.name} className="cardright__img" />
+              <div>
+                <p>
+                  {dessert.quantity} * {dessert.price}$
+                </p>
+                <span>{dessert.name}</span>
+                <span>{dessert.price * dessert.quantity}$</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
